97b361da6e6d2bfaf8ff94c6e69213810b98dd9c
roconnell15 <richard.oconnell15@gmail.com>
2024-09-05 13:18:53 +0000

    push for ASD

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -60,7 +60,10 @@ $(document).ready(function () {
   }
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data){
+    setBackgroundWithObject(data)
+    animationDetails.displayType = 1
+  }
 
   // TODO 4-a: add a function that handles the good display type
   
@@ -74,7 +77,7 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex])
   }
 
   function goodDisplay() {
